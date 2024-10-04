d8ef44da0f095a6410c97aeb77a945bcef76920c
Dungeon-master06 <qet4upes@gmail.com>
2024-10-04 18:42:20 +0600

    first commit

---
+++ qwerty.py
@@ -0,0 +1,45 @@
+# for i in range(10,0,-1):
+#     print(i)
+# print('Старт')
+
+# for i in range(1,10):
+#     print(f'9 * {i} = {9*i}')
+
+# a = 0
+# for i in range(1,101):
+#     a += i
+# print(a)
+
+# num = 12345
+# num = list(str(num))
+# num2 = 0
+# for i in num:
+#     num2 += int(i)
+#     print(num2)
+
+# text = ['hello','how','are','u']
+# # count = 0
+# for i in text:
+# #     count+=len(i)
+# # print(count)
+#     print(len(i))
+
+# alfavit = 'aeouiyAEUOIY'
+# word = 'hello world,I LOVE METALLL EyAAAAAAAAA'
+# count = 0
+# for i in alfavit:
+#     if i in word:
+#         count +=1
+# print(count)
+
+# count = 0
+# while True:
+#     num = int(input())
+#     count += num
+#     print(count)
+#     if num == 0:
+#         break
+
+num = int(input())
+for i in range(num,0,-1):
+    print(i)
